2c111d8715ec765a59a9b8c0c7e0c046c833d7f8
bbonf <b.bonfil@uu.nl>
2022-02-01 10:43:58 +0100

    tiny fix in comments

--- jspsych-uil-session.js
+++ jspsych-uil-session.js
@@ -49,7 +49,7 @@ else
              * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
              * @param {string} method - HTTP method (e.g. GET/POS)
              * @param {string} data - Data to be sent to the server, in plain text
-             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server}
+             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
              */
             _request(url, method, data) {
                 let params = {
@@ -68,7 +68,7 @@ else
             /**
              * Performs an HTTP GET request
              * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
-             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server}
+             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
              */
             _get(url) {
                 return this._request(url, 'GET');
@@ -78,7 +78,7 @@ else
              * Performs an HTTP POST request
              * @param {string} url - URL for the request, relative to the base URL used when initializing the API class
              * @param {string} data - Data to be sent to the server, in plain text
-             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server}
+             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
              */
             _post(url, data) {
                 return this._request(url, 'POST', data);
@@ -87,7 +87,7 @@ else
             /**
              * Start a new participant session on the server
              * @param {string} access_key - Access key for the experiment
-             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server}
+             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
              */
             sessionStart(access_key) {
                 return this._post(`${access_key}/participant/`);
@@ -96,7 +96,7 @@ else
             /**
              * Start a new participant session on the server
              * @param {string} access_key - Access key for the experiment
-             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server}
+             * @returns {Promise<Object>} a promise that contains the parsed JSON returned from the server
              */
             sessionUpload(access_key, session_id, data) {
                 return this._post(`${access_key}/upload/${session_id}/`, data);
