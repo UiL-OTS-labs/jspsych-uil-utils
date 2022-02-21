b030777b1eb66aa245f5891c7307828ecf9af6b3
Maarten <m.j.a.duijndam@uu.nl>
2022-02-21 14:38:42 +0100

    Random: add support for multiple constraints

--- jspsych-uil-randomization.js
+++ jspsych-uil-randomization.js
@@ -32,6 +32,113 @@ else
 
     uil.randomization = {};
 
+    /**
+     * Checks whether an item may be appended to the randomized items
+     *
+     * @param randomized {array.<object>} A pre-existing list of stimuli
+     * @param constraints {object} The constraints used to examine whether an
+     *                             item can be appended to randomized. The keys
+     *                             need to be a key in the item parameter and
+     *                             the value is the maximum number allowable
+     *                             subsequent item[key] with the same value.
+     * @param item {object} The object which is intended to be appended. The
+     *                      constraints determine whether it fits.
+     *
+     * @return true if the item may be appended to randomized without violating
+     *         the constraints false otherwise.
+     */
+    function allowPushItem (
+        randomized,
+        constraints,
+        item
+    ) {
+        for (const [key, max] of Object.entries(constraints)) {
+            if (max < 1) {
+                throw new RangeError(
+                    "max is < 1; it's impossible to fit less than one repeating items"
+                );
+            }
+            let value = item[key];
+            if (value === undefined) {
+                throw ReferenceError(
+                    `There is no key "${key}" in item "${item}"`
+                );
+            }
+
+            let selection = randomized.slice(-max);
+            selection.push(item);
+
+            let count = 0;
+            selection.forEach(stimulus => count += stimulus[key] === value);
+            if (count > max)
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Randomize the input stimuli, according to the given constraints.
+     *
+     * A new list of stimuli will be returned if reasonably possible.
+     * The constraints are an object with a key that must also be present in
+     * the original stimuli. The value of the belonging to the key, denotes
+     * how many items with item[key] may have the same value in a row.
+     *
+     * @param original_stimuli {array.<object>} The unrandomized stimuli
+     * @param constraints {object} The constraints to determine
+     * how many items with the same value may be appended in a row.
+     * @param nth_try {number} The nth attempt to randomize the stimuli [0, max_tries)
+     * @param max_tries {number} The total number of attempts to randomize the stimuli.
+     *
+     * @returns {null|[]} The randomized order. Null if randomization
+     * failed, in which case an error will have been logged to the console
+     */
+    function randomizePrivate(
+        original_stimuli,
+        constraints,
+        nth_try,
+        max_tries
+    ) {
+        if (max_tries < 1) {
+            throw new RangeError("max_tries is < 1");
+        }
+        if (nth_try < 0) {
+            throw new RangeError("nth_try is < 0");
+        }
+
+        if (nth_try >= max_tries) {
+            console.log(
+                "Unable to randomize according the constraints, "       +
+                "Perhaps it is a good idea to make the constraints "    +
+                "less strict."
+            );
+            return null;
+        }
+
+        let stimuli = Array.from(original_stimuli);
+        let order = [];
+        let item_attempts = 0; // Number of attempts to find a fitting stimulus
+
+        // Pick a random fitting input stimulus and append it to the output
+        // until no stimuli are left.
+        while (stimuli.length > 0) {
+            if (item_attempts === stimuli.length * 2) {
+                return randomizePrivate(original_stimuli, constraints, nth_try + 1, max_tries);
+            }
+            let rand_index = Math.floor(Math.random() * stimuli.length);
+            let item = stimuli[rand_index];
+            if (allowPushItem(order, constraints, item)) {
+                order.push(item);                         // push fitting item to output
+                stimuli.splice(rand_index, 1); // and remove it from the input
+                item_attempts = 0;
+            }
+            else {
+                item_attempts += 1;
+            }
+        }
+        return order;
+    }
+
     (function (context) {
 
         /**
@@ -53,7 +160,6 @@ else
          * that is allowed appear in succession
          * @param type_key {string} The name of the variable that denotes the
          * stimuli's type
-         * @param attempt {int} Internal recursion counter. DO NOT SET YOURSELF
          *
          * @returns {null|[]} The randomized order. Null if randomization
          * failed, in which case an error will have been logged to the console
@@ -61,79 +167,41 @@ else
         context.randomizeStimuli = function(
             original_stimuli,
             max_same_type = 2,
-            type_key = 'item_type',
-            attempt = 0,
+            type_key = 'item_type'
         ) {
-            // Stop after 10 randomize attempts, it's probably hopeless.
-            if(attempt > 10) {
-                console.error("Could not shuffle questions! This is most " +
-                    "likely because there is no valid order to be made with " +
-                    "the current max_same_type value!");
-                return null;
-            }
+            let constraints = {[type_key] : max_same_type}; // ES6 dependency.
+            return randomizePrivate(original_stimuli, constraints, 0, 10);
+        }
 
-            let stimuli = Array.from(original_stimuli)
+        context.randomizeStimuliConstraints = function (
+            original_stimuli,
+            constraints = {'item_type' : 2},
+            max_tries = 10
+        ) {
+            return randomizePrivate(original_stimuli, constraints, 0, max_tries);
+        }
 
-            let numItems = stimuli.length;
-            // Array to built the new order in.
-            let order = [];
-            let lastType = undefined;
-            let numOfLastType = 0;
-            // Number of attempts to find a fitting element. (NOT the amount
-            // of attempts to create a random order).
-            let attempts = 0;
-
-            while (stimuli.length > 0) {
-                // Re-try if we have more attempts to find a fitting question
-                // than twice all the questions.
-                // In this case, the randomize algorithm has made previous
-                // choices which prohibit finishing.
-                if (attempts === (numItems * 2)) {
-                    return context.randomizeStimuli(
-                        original_stimuli,
-                        max_same_type,
-                        type_key,
-                        attempt+ 1
+        /**
+         * Tests whether a shuffled array is meeting the constraints.
+         *
+         * @param stimuli {Array.<object>}
+         * @param constraints {Object}
+         */
+        context.stimuliMeetConstraints = function(stimuli, constraints) {
+            let valid = true;
+            stimuli.forEach((element, index, array) => {
+                for (const [key, max] of Object.entries(constraints)) {
+                    const subselection = array.slice(
+                        Math.max(0, index - max),
+                        index
                     );
+                    let count = 0;
+                    subselection.forEach(item => {count += (item[key] === element[key])});
+                    if (count >= max)
+                        valid = false;
                 }
-
-                // Pick a random item
-                let item = stimuli[Math.floor(Math.random()*stimuli.length)];
-                if (typeof item[type_key] === 'undefined') {
-                    console.error("The following item has no type:", item);
-                    return null;
-                }
-
-                let itemType = item[type_key];
-
-                // If this element belongs to a different group than last OR we
-                // haven't reached the limit of successive items
-                if (itemType !== lastType ||
-                    (itemType === lastType && numOfLastType < max_same_type)) {
-                    // Add it to the order
-                    order.push(item);
-                    // Remove it from the original list
-                    stimuli.splice(stimuli.indexOf(item), 1);
-
-                    // If this group isn't the same as last, reset
-                    if (itemType !== lastType) {
-                        numOfLastType = 1;
-                    }
-                    // Otherwise, increase the counter
-                    else {
-                        numOfLastType += 1;
-                    }
-                    // Reset these variables to the proper value
-                    lastType = itemType;
-                    attempts = 0;
-                } else {
-                    // This element doesn't fit. Count this as an attempt to
-                    // find a fitting element
-                    attempts += 1;
-                }
-            }
-
-            return order;
+            });
+            return valid;
         }
     })(uil.randomization)
 }
