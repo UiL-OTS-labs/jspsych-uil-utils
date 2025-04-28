47ccc7ba338fdfd820a0e260989042e8f4c776e8
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-04-28 15:34:40 +0200

    Add sphinx_js as extension to use JSDoc
    By installing sphinx_js we can use JSDoc inside sphinx and extract
    some of the information there.

--- doc/conf.py
+++ doc/conf.py
@@ -6,22 +6,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'jspsych-uil-utils'
-copyright = '2025, Ilslab'
-author = 'Ilslab'
+project = "jspsych-uil-utils"
+copyright = "2025, Ilslab"
+author = "Ilslab"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx_js"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# -- To configure stuff for JSDoc -------------------------------------------------
+
+# setup path to javascript source
+js_source_path = "../"
+
+# We might need to uncomment the following for typescript (when necessary)
+# js_language = 'typescript'
