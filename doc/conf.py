d40c2c6662919f2178ebe5c1c83177d1624b0782
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2025-04-29 12:35:22 +0200

    Start using read-the-docs theme

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx_js",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -26,7 +27,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 # # -- To configure stuff for JSDoc -------------------------------------------------
